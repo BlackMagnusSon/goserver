f0c2051a5c061108f88b078590674ea43b44394f
blackmagnusson <snimko2990@gmail.com>
2022-12-26 09:09:19 +0300

    import cobra

--- main.go
+++ main.go
@@ -4,10 +4,13 @@ import (
 	"fmt"
 	server "goserver/cmd/httpserver"
 	"goserver/cmd/logs"
+
+	"github.com/spf13/cobra"
 )
 
 func main() {
 	fmt.Println("Server Started")
 	logs.SetLogs()
+	cobra.AddTemplateFunc
 	server.HTTPserver()
 }
